26f7618a00e4cac12cf83efec95b86b64c210d57
ndanyikevin <ndanyikevin@gmail.com>
2026-08-11 20:47:52 +0300

    updated api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import { handle } from 'hono/vercel'
-import app from '../src/index' // Path to your primary Hono app instance
+import app from '../server/src/index' // Path to your primary Hono app instance
 
 // Optional: Use Edge runtime for lower latency
 export const config = {
   runtime: 'edge',
 }
 
-export default handle(app)
+export default handle(app as any)
